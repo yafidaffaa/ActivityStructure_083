15621faf9b4f1e13ef28a0ef4c1b57f722effbdf
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-04-05 11:08:01 +0700

    memasukan data mhs dan menampilkan data

--- latihan2/latihan2.cpp
+++ latihan2/latihan2.cpp
@@ -9,8 +9,33 @@ struct DetailAlamat
 
 struct Mahasiswa
 {
-	string nim;
-	string nama;
+	char nim[12];
+	char nama[20];
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukan NIM : ";
+	cin.getline(mhs.nim,12);
+	cout << "Masukan Umur : ";
+	cin >> mhs.umur;
+	cin.ignore(1, '\n');
+	cout << "Masukan Nama : ";
+	cin.getline(mhs.nama,20);
+	cout << "Alamat : ";
+	cout << "\n\tMasukan Desa : ";
+	cin.getline(mhs.alamat.desa, 20);
+	cout << "\tMasukan kota : ";
+	cin.getline(mhs.alamat.kota, 20);
+
+	cout << "\n\nNIM : " << mhs.nim;
+	cout << "\nUmur : " << mhs.umur;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\nAlamat : ";
+	cout << "\n\tDesa : " << mhs.alamat.desa;
+	cout << "\n\tDesa : " << mhs.alamat.kota;
+	
+}
